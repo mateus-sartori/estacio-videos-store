2dcea1c5a5a9fa56b7fc2c929219b2522379b5c6
Mateus <mateus-sartori@hotmail.com>
2021-08-23 00:54:52 -0400

    Scrap

--- ruby-env/local_server.rb
+++ ruby-env/local_server.rb
@@ -1,10 +1,12 @@
-require 'sinatra'
-require 'sinatra/reloader' if development?
-require_relative 'config/application'
+require "sinatra"
+require "sinatra/reloader" if development?
+require_relative "config/application"
+require "open-uri"
+require "fileutils"
 
-require_relative 'src/scrap_stacio'
+require_relative "src/scrap_stacio"
 include ScrapStacio
 
-get '/' do
-    ScrapStacio.main
+get "/" do
+  ScrapStacio.vimeo
 end
